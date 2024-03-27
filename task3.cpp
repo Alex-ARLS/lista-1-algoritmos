dac4f23a877c811b166a6c6ce08d8c8864567400
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-03-27 08:59:56 -0300

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/* Leia o valor de uma compra nas Lojas Sua Melhor
+ Compra e mostre o valor das prestações de
+acordo com a quantidade de parcelas desejada pelo usuário.
+ A loja está vendendo seus produtos
+a vista ou parcelado em até 10 vezes sem juros. */
+
+using namespace std;
+int main (){
+    
+}
