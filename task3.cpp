8608397f5204ad716726109081221072faa50ca7
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-03-31 19:36:42 -0300

    Update task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+/*Leia o valor de uma compra nas Lojas Sua Melhor Compra e mostre o valor das prestações de 
+acordo com a quantidade de parcelas desejada pelo usuário. A loja está vendendo seus produtos 
+a vista ou parcelado em até 10 vezes sem juros.*/
+
+using namespace std;
+int main (){
+    float valorCompra;
+    int prestacoes;
+
+    cout <<"olá seja bem vindo as lojas sua melhor compra \n";
+    cout <<"qual valor do produto que você deseja?";
+    cin >> valorCompra;
+
+    cout << "em quantas prestacoes vc deseja realizar esta compra?";
+    cin >> prestacoes;
+
+    
+}
