343151c53e6e5a7ef8b4c2ab098c9b28ef1b778c
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-02 12:32:33 -0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,18 +1,21 @@
-/*Leia um número inteiro N e um percentual qualquer e escreva 
-qual o valor do percentual aplicado ao número. 
-*/
+/*Leia  um  número  inteiro  N  e  um  percentual  qualquer
+e  escreva  qual  o  valor  do  percentual 
+aplicado ao número.*/
+
 #include <iostream>
+
 using namespace std;
-int main (){
-    int n, nFinal;
-    float perc, perc;
+int main(){
+    int number;
+    float percent;
+    
+    cout <<"digite um numero que seja referente ao valor que vc deseja ter o percentual; ";
+    cin >> number;
+    cout << "digite o percentual a ser aplicado em (%): ";
+    cin >> percent;
 
-    cout<< "digite um numero inteiro e um percentual a ser aplicado;\n";
-    cin << n << perc;
+    float resultado = calcularPercent(number, percent);
 
-    //aqui vai haver o processamento das entradas
-    nFinal = n + (n*(perc/100)); //o cálculo que é o processar dos dados
-    cout << "o percentual" << perc << " aplicado a "<< n <<"e" << nFinal;
-    cout << "fim do programa";
-    return 0;
+    cout << "o valor de percentual é " << percent << "% aplicado ao numero insirido que foi" << number; <<"é"<< resultado<< endl;
+
 }
