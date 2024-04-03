d282dba4fc37a731925b213e83ed7b3c85039645
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-03 20:31:30 -0300

    task 6 add

--- task6.cpp
+++ task6.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int dia, mes, ano;
+    double imposto, taxa_distribuidor;
+    float valor_carro, valor_total;
+
+    cout<< "qual o custo de producao esse carro teve";
+    cin >> valor_carro;
+
+    cout <<"uau, voce acaba de produzir esse carro no melhor pais para ser produzido, parabens\n voce acaba de receber uma taxa de 28% do distribuidor e de 45% do estado, apenas em impostos, voce e muito inteligente!";
+    valor_total = imposto
     
 }
