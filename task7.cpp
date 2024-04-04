6564b7282461c2cc1d467d4c4ec75d129236108e
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-04 09:57:32 -0300

    Create task7.cpp

---
+++ task7.cpp
@@ -0,0 +1,11 @@
+/*Leia  uma  temperatura  em  graus  Celsius  e  apresente-a  convertida  em  graus  Fahrenheit.  A 
+fórmula de conversão é F=(9*C+160)/5, sendo F a temperatura em Fahrenheit e C a temperatura 
+em Celsius.*/
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char** argv) {
+    
+
+    return 0;
+}
