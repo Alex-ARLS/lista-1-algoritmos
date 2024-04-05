b44c038a2fb60e480fa591a50f65f832521f609c
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-05 15:02:48 -0300

    ok

--- task8.cpp
+++ task8.cpp
@@ -2,6 +2,20 @@
 valor da cotação do dólar e também a quantidade de dólares que o usuário deseja converter. */
 #include <iostream>
 using namespace std;
+
 int main (){
-    
+    float dollar, real;
+    double cota;
+
+    cout << "qual o valor do dollar hoje?";
+    cin >> cota;
+
+    cout << "qual o valor vc deseja converter diretamente?";
+    cin >> dollar;
+
+    real = dollar * cota;
+
+    cout <<"o valor que esta na cotacao atual de hoje e de : " << real;
+
+    return 0;
 }
