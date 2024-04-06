6416c65c5b2cde203d8eab4ba06a596407c2f88b
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-06 15:27:01 -0300

    Update task9.cpp

--- task9.cpp
+++ task9.cpp
@@ -1,4 +1,16 @@
 /*Leia  dois  valores  inteiros  nas  variáveis  Val1  e  Val2,  troque  os  seus  conteúdos  e  escreva  o 
 resultado. Exemplo: Se o valor lido foi 10 para a variável Val1 e 11 para a variável Val2, depois 
 da troca o algoritmo terá que escreva Val1 = 11 e Val2 = 10. */
+#include <iostream>
+
+using namespace std;
+int main (){
+    int var1;
+    int var2;
+    
+        
+    
+
+
+}
 
