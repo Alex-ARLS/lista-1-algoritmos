168913f37e7ea2a7cc87bcd67fe1630bb865763b
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-09 13:34:45 -0300

    Create task29.cpp

---
+++ task29.cpp
@@ -0,0 +1,30 @@
+/*VElabore um algoritmo que mostre o calendário de um determinado mês. Os dados de entrada são o mês 
+(cujos dados devem ser mostrados) e o dia da semana em que esse mês inicia. Assuma que esse mês está 
+em  um  ano  que  não  é  bissexto,  porém  considere  meses  de  28,  30  e  31.  O  resultado  deve  ser  como 
+mostrado abaixo. Ex: janeiro, começando na 5a feira*/
+#include <iostream>
+
+using namespace std;
+
+int int main() {
+    //dia inicial 1 para doming, 2 para segunda,...
+    int mes, dia;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
