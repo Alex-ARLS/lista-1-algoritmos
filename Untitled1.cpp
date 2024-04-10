31f830a3c124d896a2a2e30392f3e930ddad799b
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-10 18:03:10 -0300

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,28 +1,21 @@
-#include <iostream>
-
+ #include <iostream>
 using namespace std;
 
 int main() {
-    int Val1, Val2, temp;
+  int myAge;
+  int votingAge;
+  
+  cout<<"how old are you?";
+  cin >> myAge;
+  
+  cout << "qual a idade minima para votar?";
+  cin >>votingAge;
 
-    // Leitura dos valores
-    cout << "Digite o valor para Val1: ";
-    cin >> Val1;
-
-    cout << "Digite o valor para Val2: ";
-    cin >> Val2;
-
-    // Troca dos valores usando uma variável temporária
-    temp = Val1;
-    Val1 = Val2;
-    Val2 = temp;
-
-    // Exibição dos valores trocados
-    cout << "\nDepois da troca:\n";
-    cout << "Val1 = " << Val1 << endl;
-    cout << "Val2 = " << Val2 << endl;
-
-    return 0;
+  if (myAge >= votingAge) {
+    cout << "Old enough to vote!";
+  } else {
+    cout << "Not old enough to vote.";
+  }
+  return 0;
 }
-
 
