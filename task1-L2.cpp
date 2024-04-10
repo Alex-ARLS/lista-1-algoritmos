388855d2cb1a1329bf8ac2d4c70c33066f45c94e
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-10 13:27:32 -0300

    Create task1-L2.cpp

---
+++ task1-L2.cpp
@@ -0,0 +1,32 @@
+/*Dados  três  valores  X,  Y  e  Z,  verifique  e  escreva  se  eles  podem  ser  os  comprimentos  dos  lados  de  um 
+triângulo e, se forem, verificar e escrever se é  um triângulo equilátero, isósceles ou escalenos. Se eles 
+não formarem um triângulo, escrever esta mensagem. Considere as seguintes propriedades:   
+• O comprimento de cada lado em um triângulo é menor que a soma dos outros dois lados;   
+• Equiláteros: tem os comprimentos dos três lados iguais;   
+• Isósceles: tem os comprimentos de dois lados iguais;  •  Escaleno: tem os comprimentos dos três 
+
+    triangulo;cada lado é menor que a soma dos outros dois lados.
+    equilateros;tem os comprimentos dos três lados iguais.
+    isósceles; tem os comprimentos de dois lados iguais e escalenos tem os comprimentos dos tres.
+    lados diferentes. 
+
+
+
+
+
+*/
+#include <iostream>
+using namespace std;
+int main (){
+    float lado1, lado2, lado3;
+
+    cout << "qual valor do lado 1?";
+    cin >> lado1;
+    cout << "qual valor do lado 2?";
+    cin >> lado2;
+    cout << "qual valor do lado 3?";
+    cin >> lado3;
+    
+
+
+}
