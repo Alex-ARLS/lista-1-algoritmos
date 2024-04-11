4af1e11cb16c21fa0a56bfe638632d78156ac29c
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-10 21:08:36 -0300

    +1

--- task1-L2.cpp
+++ task1-L2.cpp
@@ -6,9 +6,12 @@ não formarem um triângulo, escrever esta mensagem. Considere as seguintes prop
 • Isósceles: tem os comprimentos de dois lados iguais;  •  Escaleno: tem os comprimentos dos três 
 
     triangulo;cada lado é menor que a soma dos outros dois lados.
+
     equilateros;tem os comprimentos dos três lados iguais.
-    isósceles; tem os comprimentos de dois lados iguais e escalenos tem os comprimentos dos tres.
-    lados diferentes. 
+
+    isósceles; tem os comprimentos de dois lados iguais e 
+    
+    escalenos tem os comprimentos dos tres lados diferentes. 
 */
 #include <iostream>
 using namespace std;
@@ -24,18 +27,20 @@ int main (){
     cout << "qual valor do lado 3?";
     cin >> lado3;
 
-    if ((lado1 <= lado2 + lado3)&&(lado2 <= lado1 + lado3)&&(lado3 <= lado1 + lado2))
-    {
-        cout <<"bom... a soma dos dois lados, é maior que um lado! por isso não é um triangulo.";
-    }else(
+    if ((lado1 <= lado2 + lado3)&&(lado2 <= lado1 + lado3)&&(lado3 <= lado1 + lado2)){
+        cout <<"bom... a soma dos dois lados, e maior que um lado! por isso não  um triangulo."; //triangulo
+    } else{
         cout <<"os valores que você digitou caracterizam um triangulo, parabens, amigo!";
-    )
 
-    
-    
+    } if((lado1 == lado2)&&(lado2 == lado3)&&(lado3 == lado1)){
+        cout << "parabens vc acaba de escrever dimensoes de um equilatero, eles possuem lados exatamente iguais.";
 
+    } else if((lado1 == lado2)&&(lado2 == lado1)&&(lado3 != lado2)&&(lado1 != lado2)){
+        cout <<"parabens vc acaba digitar dimensoes de um isosceles";
 
-    
+    } else {
+        cout<<"parabens vc acaba de digitar as dimensoes de um escaleno";
+    }
 
-
+    return 0;
 }
---
+++ task2-L2.cpp
@@ -0,0 +1,14 @@
+/*Leia um número, verifique e escreva se este número é par ou ímpar.
+ Se for par, verifique e escreva se é 
+maior que 100 ou não e se for ímpar verifique
+ e escreva se é positivo ou negativo. */
+ #include <iostream>
+
+ using namespace std;
+ int main(){
+    int par, impar;
+    cout << "digite um numero para decifrarmos";
+    cin >> par, impar;
+    
+
+ }
