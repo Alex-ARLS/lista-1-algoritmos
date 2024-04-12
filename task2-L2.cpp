4f061d9b11ffe87571168bfe4c70dfc63eb30556
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-11 21:39:26 -0300

    +1

--- task2-L2.cpp
+++ task2-L2.cpp
@@ -1,11 +1,17 @@
+/*Leia um número, verifique e escreva se este número é par ou ímpar. 
+Se for par, verifique e escreva se é 
+maior que 100 ou não e se for ímpar verifique e escreva se é positivo ou negativo.*/
+
 #include <iostream>
 using namespace std;
 int main(){
   int numero;
-  bool par;
-  bool impar;
 
-  cout<< "digite o valor que vc quer processar como par ou impar!\n";
+  bool par;
+
+  bool impar = false;
+
+  cout << "digite o valor que vc quer processar como par ou impar!\n";
   cin >> numero;
 
   if (numero % 2 == 0)
@@ -13,17 +19,17 @@ int main(){
     par = true;
     cout <<"o valor eh par.\n";
   }else{
-    impar=true;
+    impar = true;
     cout <<"o valor eh impar.\n";
   }
-  if (numero > 100){
-    cout <<"o numero eh maior que 100";
+  if ((numero > 100)){
+    cout << "o numero eh maior que 100";
   }else{
     cout << "o numero eh menor que 100";
   }
   
-  
-
+      
+  return 0;
 
 
 }
