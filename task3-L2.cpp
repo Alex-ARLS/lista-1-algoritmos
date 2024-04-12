566c5e7a09824b9775c5674de7c66099666e1e7e
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-11 22:03:47 -0300

    Update task3-L2.cpp

--- task3-L2.cpp
+++ task3-L2.cpp
@@ -1,11 +1,12 @@
 /*Calcule o peso ideal de uma pessoa. Dados de entrada: altura e gênero (“m”-masculino ou “f”-feminino). 
 Utilize as seguintes fórmulas para cálculo do peso ideal:   
-• Masculino = (72,7 x altura) - 58   
-• Feminino = (62,1 x altura) - 44,7 */
+• Masculino = (72,7 x altura) - 58   case 1
+• Feminino = (62,1 x altura) - 44,7 case 2 */
 #include <iostream>
 
 using namespace std;
 int main(){
+    
     
     
 
