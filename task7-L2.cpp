54c2172f3edce5f846be0ce340f17f4e8d1ac73a
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-15 14:26:23 -0300

    Update task7-L2.cpp

--- task7-L2.cpp
+++ task7-L2.cpp
@@ -19,6 +19,54 @@ de cada um e calcule o valor a ser pago por aquele lanche.*/
 #include <iostream>
 using namespace std;
 int main (){
-    int cardCod;
+    int codSanduiche, codBebida, qtdBebida, qtdSanduiche;
+
+    float totalConta;
+
+    do
+    {   
+            switch (codSanduiche)
+            {
+            case 100:
+                cout <<"cachorro quente adicionado.";
+                cin >> qtdSanduiche;
+                totalConta += qtdSanduiche;
+                break;
+                            case 101:
+                cout <<"bauro simples adicionado";
+                totalConta += qtdSanduiche;
+                break;
+                            case 102:
+                cout <<"cachorro quente adicionado.";   
+                totalConta += qtdSanduiche;             
+                break;
+                            case 103:
+                cout <<"cachorro quente adicionado.";   
+                totalConta += qtdSanduiche;             
+                break;
+                            case 104:                
+                cout <<"cachorro quente adicionado.";
+                totalConta += qtdSanduiche;
+                
+                break;    
+            
+            default:
+                cout << "nenhum item selecionado conforme o codigo de sanduiche informado";
+                break;
+            }
+
+            switch (codBebida)
+            {
+            case 105:
+                totalConta += qtdBebida;
+                cout<<"qu"
+                break;
+            
+            default:
+                break;
+            }
+        
+
+    }
     
 }
