cb38a68cffbdcaac5acc5e0165379f16cc7a8c9d
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-04-15 11:33:58 -0300

    Update task6-L2.cpp

--- task6-L2.cpp
+++ task6-L2.cpp
@@ -1,6 +1,6 @@
-/*A escola “APRENDER” faz o pagamento de seus professores por hora/aula.
+/*A escola “APRENDER” faz o pagamento* de seus professores* por hora/aula.
  Faça um algoritmo que calcule 
-e escreva o salário de um professor, 
+e escreva o salário* de um professor, 
 sabendo que o valor da hora/aula segue a tabela abaixo:  
  
 Professor nível 1  R$ 12,00 por hora aula  
@@ -12,5 +12,28 @@ Professor nível 3  R$ 25,00 por hora aula */
 
 using namespace std;
 int main(){
+    int professorLevel;
+    float horaAula;
 
+    cout<<"qual o nivel do professor que vc deseja calcular?";
+    cin>> professorLevel;
+
+    switch (professorLevel)
+    {
+    case 1:
+    horaAula = 12.00;
+        cout <<"o nivel do qual vc inseriu e igual a:" << horaAula;
+        break;
+        case 2:
+    horaAula = 17.00;
+        cout <<"o nivel do qual vc inseriu e igual a:" << horaAula;
+        break;
+        case 3:
+    horaAula = 25.00;
+        cout <<"o nivel do qual vc inseriu e igual a:" << horaAula;
+        break;
+    
+    default:
+        break;
+    }
 }
