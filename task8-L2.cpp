2c711f9fe2fc99a3550af62b3214977e7bd00bdb
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-05-20 11:19:14 -0300

    Update task8-L2.cpp

--- task8-L2.cpp
+++ task8-L2.cpp
@@ -3,5 +3,30 @@ mostre a soma dos seus próximos 10 antecessores ou sucessores de acordo com a l
 #include <iostream>
 using namespace std;
 int main (){
-    
+    int numero;
+    char opcao;
+
+    cout << "digite um numero inteiro; ";
+    cin >> numero;
+    cout << "digite 'a' para calcular a soma dos 10 antecessores ou 's' para os sucessores:";
+    cin >> opcao;
+
+    int soma = 0;
+
+    if ((opcao == 'a')|| (opcao == 'A')){
+        //isso vai ser a soma dos 10 antecessores
+        for (int i = 1; i <= 10; ++i){
+            soma += (numero - i);
+        }
+        cout << "A soma dos 10 antecessores de " << numero << " é :" << soma;
+    } else if (( opcao == 's')|| (opcao =='S')){
+        for (int i = 1; i<=10; ++i) {
+            soma += (numero + i );
+        }
+    cout << "A soma dos 10 sucessores de " << numero << " é: " << soma << std::endl;
+
+    }else {
+        cout << "opcao invalida!";
+    }
+    return 0;
 }
